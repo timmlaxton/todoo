ae27841ededd798bb6fc3da57fb93f99493a2d75
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-08-08 14:51:08 +0100

    added transitioned to delteing item

--- script.js
+++ script.js
@@ -35,7 +35,10 @@ function deleteCheck(e) {
   const item = e.target;
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
-    todo.remove();
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
   }
 
   if (item.classList[0] === "completed-btn") {
